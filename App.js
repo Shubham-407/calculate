74a141230fd259912208f7cf1871cb74d7d38c16
Shubham-407 <108684617+Shubham-407@users.noreply.github.com>
2022-08-26 00:48:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
-
+let keys = document.querySelectorAll("#keyboard>div")
+    let bag=""
+    for(let i=0;i<keys.length;i++){
+        keys[i].addEventListener("click",myCal)
+    }
+    function myCal(){
+        let target = event.target.innerText
+        if(target == "C"){
+            document.querySelector("#display").innerText = ""
+            bag =""
+        }
+        else if (target == "="){
+            console.log(eval(bag))
+            let res = eval(bag)
+            document.querySelector("#display").innerText = res
+        }
+        else{
+            bag=bag+target
+        document.querySelector("#display").innerText = bag
+        } 
+    }
